427aaf5e4391d80bca31223d97686a842e7ee584
Michal Piotrowski <practicalrs.com@gmail.com>
2024-02-18 22:51:22 +0100

    #2 data generator p1
     #2 data generator p2
    
     #2 data generator p3
    
     #2 data generator p4

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+use rand::Rng;
+use std::{env, fs::read_to_string, fs::OpenOptions, io::Write};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[derive(Debug)]
+pub enum Error {
+    CommandLine,
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error!")
+    }
+}
+
+pub fn run() -> Result<()> {
+    let limit = 1000000000;
+    let args: Vec<String> = env::args().collect();
+    let file = args.last().ok_or(Error::CommandLine)?;
+    let cities = read_to_string(file)?;
+    let mut result;
+    let mut rng = rand::thread_rng();
+    let mut count = 0;
+
+    'outer: while count <= limit {
+        result = vec![];
+
+        for city in cities.split('\n') {
+            let temp = rng.gen_range(-40.0..60.0);
+            let res = format!("{city};{:.1}\n", temp);
+
+            result.push(res);
+
+            count += 1;
+
+            if count >= limit {
+                let _ = write_results(&result);
+
+                break 'outer;
+            }
+        }
+
+        let _ = write_results(&result);
+    }
+
+    Ok(())
+}
+
+fn write_results(result: &[String]) -> Result<()> {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .read(true)
+        .open("result.csv")?;
+
+    for res in result {
+        file.write_all(res.as_bytes())?;
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#![forbid(unsafe_code)]
+
 fn main() {
-    println!("Hello, world!");
+    let result = prs_1brc_cg::run();
+
+    if let Err(e) = result {
+        eprintln!("Error {e:?}");
+    }
 }
